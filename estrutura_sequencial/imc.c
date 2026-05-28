f8c1b30bb68b6ea069d46e3b6a8ef75392901569
Pablo Silva <pablo.sas22@gmail.com>
2026-05-28 08:48:48 -0300

    Cálculo IMC

---
+++ estrutura_sequencial/imc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    //Delcaração de Variáveis
+    float pesoUsuario, alturaUsuario;
+
+    //Entrada de Dados
+    printf("Digite o seu peso: ");
+    scanf("%f", &pesoUsuario);
+    prinf("Digite a sua altura: ");
+    scanf("%f", &alturaUsuario);
+
+    //Processamento de Dados
+    float imcUsuario;
+    imcUsuario = pesoUsuario / pow(alturaUsuario,2);
+
+    //Saída de Dados
+    printf("O IMC do usuario com o peso %.2fkg e altura %.2f metros, e de %.2f", pesoUsuario, alturaUsuario, imcUsuario);
+
+    printf("\n");
+    system("pause");
+
+    return 0;
+}

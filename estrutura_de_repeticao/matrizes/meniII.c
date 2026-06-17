0068ab77397bb2fdfa21aa72b219d5472ef73b53
Pablo Silva <pablo.sas22@gmail.com>
2026-06-17 08:54:59 -0300

    Depois eu vejo

--- estrutura_de_repeticao/matrizes/meniII.c
+++ estrutura_de_repeticao/matrizes/meniII.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void operacao(int num1, int num2);
+void operacaoBasica(int num1, int num2);
 
 int main(){
+ //Declaração de Variáveis
+ int operacao;
+
+ //Entrada de Dados
+ do{
+    prinft(" ---- OPERACAO ---- ");
+    printf(" 1 - SOMA\n 2 - DIVISAO\n 3 - MULTIPLICAO\n 4 - SUBTRACAO\n");
+    scanf("%d", &operacao);
+    if(operacao > 0 || operacao <= 4)
+ }while (true);
 
 
+ 
+
+  return 0;
 }
 
-
-void operacap(){
-    
-
-}
+void operacaoBasica(int num1, int num2);
 
 
 
